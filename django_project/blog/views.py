82154f3212525e7d450263ea9f42e34bead38449
Nishant ketu <nketu06@gmail.com>
2024-04-29 20:09:55 +0530

    add post detail-using class view

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,12 +1,22 @@
 from django.shortcuts import render
 from . models import Post
+from django.views.generic import ListView,DetailView
 
 
-def home(request):
-    context = {
-        'posts': Post.objects.all()
-    }
-    return render(request, 'blog/home.html', context)
+# def home(request):
+#     context = {
+#         'posts': Post.objects.all()
+#     }
+#     return render(request, 'blog/home.html', context)
 
 def about(request):
     return render(request, 'blog/about.html', {'title': 'About'})
+
+class PostListView(ListView):
+    model=Post
+    template_name='blog/home.html'
+    context_object_name='posts'
+    ordering=['-date_posted']
+
+class PostDetailView(DetailView):
+    model=Post

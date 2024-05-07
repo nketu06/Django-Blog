6aad76951c6d547095925cd80716a9fa5fbdcc0e
Nishant ketu <nketu06@gmail.com>
2024-05-08 00:41:45 +0530

    added reset password

--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -135,3 +135,13 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 LOGIN_REDIRECT_URL='blog-home'
 LOGIN_URL='login'
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+
+# use you env_var
+EMAIL_HOST_USER = 'username.com'
+EMAIL_HOST_PASSWORD = 'your_app_password'
+SERVER_EMAIL = 'username.com'
+
